d1e9d955c1d3c179b938038e85532d24125dcbb5
unknown <askuratovskiy@gmail.com>
2018-02-05 17:04:05 +0200

    Start

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Calculator {
+
+    private int result;
+
+    public void add (int ... params){
+        for (Integer param : params){
+            this.result += param;
+        }
+    }
+
+    public int getResult(){
+        return this.result;
+    }
+
+    public void cleanResult(){
+        this.result = 0;
+    }
+}
